2b2b58ae8f7a61c8c4e47a748b2668c255ce859c
harsh6768 <harshchaurasiya6768@gmail.com>
2019-10-21 12:53:49 +0530

    updatePost routes changed

--- Routes/mainRoutes.js
+++ Routes/mainRoutes.js
@@ -6,7 +6,7 @@ Router.route('/getPosts').get(postController.getPosts);
 Router.route('/getPost/:id').get(postController.getPostById);
 Router.route('/getPost/:title').get(postController.getPostByTitle);
 Router.route('/getPost/:author').get(postController.getPostByAuthor);
-Router.route('/updatePost').put(postController.updatePost);
+Router.route('/updatePost/:id').put(postController.updatePost);
 Router.route('/deletePost/:id').delete(postController.deletePost);
 
 
