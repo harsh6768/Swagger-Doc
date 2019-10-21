0fbdb2b6cb832f6286c47b29a9fb9a7e9abc8cb2
harsh6768 <harshchaurasiya6768@gmail.com>
2019-10-21 14:14:29 +0530

    swagger-doc running successful

--- endPoints.js
+++ endPoints.js
@@ -3,47 +3,14 @@ const routes=require('./Routes/mainRoutes');
 const initEndPoint=(app)=>{
 
     /**
-     * @Swagger
+     * @swagger
      * /createPost:
-     *      post:
-     *          tags:
-     *          - Post
-     *          summary: Create Post
-     *          description: This api will create new Post
-     *          consumes:
-     *          - application/json
-     *          - application/xml
-     *          produces:
-     *          - application/json
-     *          - application/xml
-     *          parameters:
-     *          - in: body
-     *            name: body
-     *            description: create post
-     *            required: true
-     *            schema:
-     *              $ref: '#/definitions/Post'
-     *          responses:
-     *            200:
-     *              description: successful operation
-     *            400:
-     *              description: bad request
-     *            500:
-     *              description: internal server error
-     * 
-     * definitions:
-     *      Post:
-     *          type: Object
-     *          properties:
-     *              title:
-     *                  type: string
-     *              body: 
-     *                  type: string
-     *              author:
-     *                  type: string
-     * 
+     *         post:
+     *           tags:
+     *           - posts
+     *           
+     *            
      */
-    
     app.use('/',routes);
 }
 
